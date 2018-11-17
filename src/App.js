277d2f11a98f210429f6974eeae130b3a5d66862
saoi <sitx00@gmail.com>
2018-11-17 17:52:16 +0300

    Test application for news output To work with json used axios Style a component with styled-component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,62 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import styled from 'styled-components'
+
+import axios from 'axios';
+
+import Article from './components/article';
+
+const AppContainer = styled.section`
+	display: flex;
+	flex-direction: column;
+	padding: 30px;
+	background-color: #002F2F;
+	min-height: 100vh;
+`;
+const SearchInput = styled.input`
+	border: 2px solid #A7A37E;
+	border-radius: 5px;
+	font-size: 14px;
+	padding: 10px;
+`;
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	state = {
+		persons: [],
+		filterString: ''
+	};
+
+	componentDidMount() {
+		axios.get(`https://jsonplaceholder.typicode.com/posts`)
+			.then(res => {
+				const persons = res.data;
+				this.setState({persons});
+			})
+	};
+
+	handelInput = () => {
+		this.setState({
+			filterString: this.search.value
+		});
+	};
+
+	render() {
+		let filtered = this.state.persons.filter(item => item.title.includes(this.state.filterString));
+		return (
+			<AppContainer>
+				<SearchInput
+					type="text"
+					onChange={this.handelInput}
+					ref={input => this.search = input}
+				/>
+				{
+					filtered.length > 0 ?
+						filtered.slice(0, 10).map(person => <Article articleData={person}/>)
+						:
+						<h3>Новостей нет</h3>
+				}
+			</AppContainer>
+		);
+	}
 }
 
 export default App;
---
+++ src/components/article.js
@@ -0,0 +1,52 @@
+import React, {Component} from 'react';
+import styled from 'styled-components'
+
+const ArticleWrapper = styled.article`
+	background-color: #046380;
+	margin-top: 15px;
+	padding: 15px;
+	border-radius: 5px;
+	color: #E6E2AF;
+	h3 {
+		margin: 0;
+	}
+`;
+const Button = styled.button`
+	background-color:#002F2F;
+	border: none;
+	border-radius: 5px;
+	font-size: 14px;
+	color: #E6E2AF;
+	padding: 5px;
+	cursor: pointer;
+`;
+
+class Article extends Component {
+	constructor(props) {
+		super(props);
+		this.state= {
+			active: false,
+		}
+	}
+
+	toggleVisible = () => {
+		this.setState({
+			active: !this.state.active
+		});
+	};
+
+	render() {
+		return (
+			<ArticleWrapper >
+				<h3>{this.props.articleData.title}</h3>
+				<p>{this.props.articleData.body}</p>
+				<Button
+					onClick={this.toggleVisible}>
+					Прочитал
+				</Button>
+			</ArticleWrapper>
+		)
+	}
+}
+
+export default Article;
